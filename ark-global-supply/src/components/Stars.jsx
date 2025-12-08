c1de8185cb840aeb1000f3f2b03387d91f74cf7b
ArkGlobalS <moonshotprotocol@protonmail.com>
2025-12-08 23:53:59 +1100

    Delete ark-global-supply directory

--- ark-global-supply/src/components/Stars.jsx
+++
@@ -1,9 +0,0 @@
-import { COLORS } from '../config';
-
-const Stars = ({ rating = 5, color = COLORS.gold }) => (
-  <span style={{ color }}>
-    {'★'.repeat(Math.floor(rating))}{'☆'.repeat(5 - Math.floor(rating))}
-  </span>
-);
-
-export default Stars;
